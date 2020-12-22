cc19c32e278663ec6216c349b115bb7f08dba50a
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2020-12-22 18:56:22 +0100

    Refactoring

--- core/src/test/java/com/ttbmp/cinehub/core/ExampleTest.java
+++ core/src/test/java/com/ttbmp/cinehub/core/ExampleTest.java
@@ -2,7 +2,7 @@ package com.ttbmp.cinehub.core;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ExampleTest {
 
