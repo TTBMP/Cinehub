63629a16d9ec946e50b2798dbdc06de19bf36d31
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2021-02-03 01:36:08 +0100

    Fix #23

--- setup_database.sh
+++ setup_database.sh
@@ -1,6 +1,3 @@
 mysql --version
-mysql -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost';"
-mysql -e "FLUSH PRIVILEGES;"
 mysql -u admin --password="admin" < ./data/local/src/main/resources/cinemadb.sql
 mysql -e 'call popola();' cinemadb
