3ca681b73fc5972333594707a93df46f748f1592
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2021-02-03 01:30:55 +0100

    Fix #23

--- setup_database.sh
+++ setup_database.sh
@@ -1,9 +1,3 @@
-wget http://repo.mysql.com/mysql-apt-config_0.8.15-1_all.deb
-dpkg -i mysql-apt-config_0.8.15-1_all.deb
-apt-get update -q
-apt-get install -q -y --allow-unauthenticated -o Dpkg::Options::=--force-confnew mysql-server
-systemctl restart mysql
-mysql_upgrade
 mysql --version
 mysql -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost';"
