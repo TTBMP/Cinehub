e69ac73ae0269094ff7941a315ee8df7f6ac0c74
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2021-02-04 03:33:23 +0100

    Fix #23

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id ("org.sonarqube") version "3.0"
+    id ("org.sonarqube") version "3.1.1"
 }
 
 buildscript {
@@ -7,7 +7,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("org.sonarsource.scanner.gradle","sonarqube-gradle-plugin","3.0")
+        classpath("org.sonarsource.scanner.gradle","sonarqube-gradle-plugin","3.1.1")
     }
 }
 
@@ -15,6 +15,8 @@ sonarqube {
     properties {
         property("sonar.sourceEncoding", "UTF-8")
         property("sonar.projectKey", "TTBMP_Cinehub")
+        property("sonar.organization", "ttbmp")
+        property("sonar.host.url", "https://sonarcloud.io")
         property("sonar.verbose", "true")
     }
 }
