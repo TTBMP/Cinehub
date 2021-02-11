3860bcfcdc14d7993c6ef6099ff905d46d0d5211
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2021-02-11 03:29:08 +0100

    Fix #34
    Rebase

--- service/authentication/src/main/java/authentication/firebase/Firebase.java
+++ service/authentication/src/main/java/authentication/firebase/Firebase.java
@@ -5,7 +5,6 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.*;
-import com.ttbmp.cinehub.app.service.authentication.LoginException;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -14,26 +13,26 @@ import java.io.IOException;
 public class Firebase {
     private FirebaseAuth mAuth;
 
-    public String loginFirebase(String email) throws LoginException {
+    public String loginFirebase(String email) throws FirebaseException {
         try {
             connection();
             return mAuth.getUserByEmail(email).getUid();
         } catch (FirebaseAuthException e) {
-           throw new LoginException("Unable to login");
+           throw new FirebaseException("Unable to login");
         }
     }
 
-    public String registerFirebase(String email, String password) throws LoginException {
+    public String registerFirebase(String email, String password) throws FirebaseException {
         try {
             connection();
             mAuth.createUser(new UserRecord.CreateRequest().setEmail(email).setPassword(password));
             return mAuth.getUserByEmail(email).getEmail();
         } catch (FirebaseAuthException e) {
-            throw new LoginException("Unable to register");
+            throw new FirebaseException("Unable to register");
         }
     }
 
-    private void connection() throws LoginException {
+    private void connection() throws FirebaseException {
         try{
         FileInputStream fis = new FileInputStream("C:\\Users\\Ivan\\Documents\\GitHub\\Cinehub\\service\\authentication\\src\\main\\resources\\apiKey.json");
         FirebaseOptions options =  FirebaseOptions.builder()
@@ -44,7 +43,7 @@ public class Firebase {
         mAuth = FirebaseAuth.getInstance();
         }
         catch (IOException e) {
-            throw new LoginException("Problem with database connection");
+            throw new FirebaseException("Problem with database connection");
 
         }
     }
---
+++ service/authentication/src/main/java/authentication/firebase/FirebaseException.java
@@ -0,0 +1,9 @@
+package authentication.firebase;
+
+public class FirebaseException extends Exception {
+
+    public FirebaseException(String message) {
+        super(message);
+    }
+
+}
