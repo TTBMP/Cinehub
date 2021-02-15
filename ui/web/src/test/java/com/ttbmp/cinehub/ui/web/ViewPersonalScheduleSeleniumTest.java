8995216afbb777c408c798932e06b0af22eb6dc7
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2021-02-15 20:27:20 +0100

    Fix #56
    Add about web page.
    Add view personal schedule selenium test.

---
+++ ui/web/src/test/java/com/ttbmp/cinehub/ui/web/ViewPersonalScheduleSeleniumTest.java
@@ -0,0 +1,54 @@
+package com.ttbmp.cinehub.ui.web;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class ViewPersonalScheduleSeleniumTest {
+
+    private WebDriver driver;
+
+    public WebElement datePicker;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("http://localhost:8080/schedule");
+        datePicker = driver.findElement(By.id("date-picker"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    void isDefaultDateTodayDate() {
+        assertEquals(
+                LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE),
+                datePicker.getAttribute("value")
+        );
+    }
+
+}
