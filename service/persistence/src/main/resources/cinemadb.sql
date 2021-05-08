29ce495a38d41a6c0b8493b8b1f099b95cf98ba6
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2021-05-08 13:33:10 +0200

    Refactory #81
    Refactory of the store procedures

--- service/persistence/src/main/resources/cinemadb.sql
+++ service/persistence/src/main/resources/cinemadb.sql
@@ -1,206 +1,224 @@
 -- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema cinemadb
--- -----------------------------------------------------
+SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
+SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
+SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
 -- Schema cinemadb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `cinemadb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `cinemadb` ;
+DROP SCHEMA IF EXISTS `cinemadb`;
+
+-- -----------------------------------------------------
+-- Schema cinemadb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `cinemadb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `cinemadb`;
 
 -- -----------------------------------------------------
 -- Table `cinemadb`.`cinema`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cinemadb`.`cinema` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NOT NULL,
-  `indirizzo` VARCHAR(45) NOT NULL,
-  `citta` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `cinemadb`.`cinema`
+(
+    `id`        INT         NOT NULL AUTO_INCREMENT,
+    `nome`      VARCHAR(45) NOT NULL,
+    `indirizzo` VARCHAR(45) NOT NULL,
+    `citta`     VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `cinemadb`.`sala`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cinemadb`.`sala` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `id_cinema` INT NOT NULL,
-  `numero` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Sala_Cinema1_idx` (`id_cinema` ASC) VISIBLE,
-  CONSTRAINT `fk_Sala_Cinema1`
-    FOREIGN KEY (`id_cinema`)
-    REFERENCES `cinemadb`.`cinema` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `cinemadb`.`sala`
+(
+    `id`        INT         NOT NULL AUTO_INCREMENT,
+    `id_cinema` INT         NOT NULL,
+    `numero`    VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_Sala_Cinema1_idx` (`id_cinema` ASC) VISIBLE,
+    CONSTRAINT `fk_Sala_Cinema1`
+        FOREIGN KEY (`id_cinema`)
+            REFERENCES `cinemadb`.`cinema` (`id`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `cinemadb`.`posto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cinemadb`.`posto` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `id_sala` INT NOT NULL,
-  `posizione` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Posto_Sala1_idx` (`id_sala` ASC) VISIBLE,
-  CONSTRAINT `fk_Posto_Sala1`
-    FOREIGN KEY (`id_sala`)
-    REFERENCES `cinemadb`.`sala` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `cinemadb`.`posto`
+(
+    `id`        INT         NOT NULL AUTO_INCREMENT,
+    `id_sala`   INT         NOT NULL,
+    `posizione` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_Posto_Sala1_idx` (`id_sala` ASC) VISIBLE,
+    CONSTRAINT `fk_Posto_Sala1`
+        FOREIGN KEY (`id_sala`)
+            REFERENCES `cinemadb`.`sala` (`id`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `cinemadb`.`film`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cinemadb`.`film` (
-  `id` INT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `cinemadb`.`film`
+(
+    `id` INT NOT NULL,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `cinemadb`.`proiezione`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cinemadb`.`proiezione` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `id_sala` INT NOT NULL,
-  `id_film` INT NOT NULL,
-  `data` DATE NOT NULL,
-  `inizio` TIME NOT NULL,
-  `prezzo_base` DOUBLE NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Proiezione_Film1_idx` (`id_film` ASC) VISIBLE,
-  INDEX `fk_Proiezione_Sala1_idx` (`id_sala` ASC) VISIBLE,
-  CONSTRAINT `fk_Proiezione_Film1`
-    FOREIGN KEY (`id_film`)
-    REFERENCES `cinemadb`.`film` (`id`),
-  CONSTRAINT `fk_Proiezione_Sala1`
-    FOREIGN KEY (`id_sala`)
-    REFERENCES `cinemadb`.`sala` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `cinemadb`.`proiezione`
+(
+    `id`          INT    NOT NULL AUTO_INCREMENT,
+    `id_sala`     INT    NOT NULL,
+    `id_film`     INT    NOT NULL,
+    `data`        DATE   NOT NULL,
+    `inizio`      TIME   NOT NULL,
+    `prezzo_base` DOUBLE NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_Proiezione_Film1_idx` (`id_film` ASC) VISIBLE,
+    INDEX `fk_Proiezione_Sala1_idx` (`id_sala` ASC) VISIBLE,
+    CONSTRAINT `fk_Proiezione_Film1`
+        FOREIGN KEY (`id_film`)
+            REFERENCES `cinemadb`.`film` (`id`),
+    CONSTRAINT `fk_Proiezione_Sala1`
+        FOREIGN KEY (`id_sala`)
+            REFERENCES `cinemadb`.`sala` (`id`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `cinemadb`.`utente`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cinemadb`.`utente` (
-  `id` VARCHAR(45) NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `cognome` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `cinemadb`.`utente`
+(
+    `id`      VARCHAR(45) NOT NULL,
+    `nome`    VARCHAR(45) NOT NULL,
+    `cognome` VARCHAR(45) NOT NULL,
+    `email`   VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `cinemadb`.`biglietto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cinemadb`.`biglietto` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `id_posto` INT NOT NULL,
-  `id_proiezione` INT NOT NULL,
-  `id_utente` VARCHAR(45) NOT NULL,
-  `prezzo` DOUBLE NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Biglietto_Utente1_idx` (`id_utente` ASC) VISIBLE,
-  INDEX `fk_Biglietto_Posto1_idx` (`id_posto` ASC) VISIBLE,
-  INDEX `fk_Biglietto_Proiezione1_idx` (`id_proiezione` ASC) VISIBLE,
-  CONSTRAINT `fk_Biglietto_Posto1`
-    FOREIGN KEY (`id_posto`)
-    REFERENCES `cinemadb`.`posto` (`id`),
-  CONSTRAINT `fk_Biglietto_Proiezione1`
-    FOREIGN KEY (`id_proiezione`)
-    REFERENCES `cinemadb`.`proiezione` (`id`),
-  CONSTRAINT `fk_Biglietto_Utente1`
-    FOREIGN KEY (`id_utente`)
-    REFERENCES `cinemadb`.`utente` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `cinemadb`.`biglietto`
+(
+    `id`            INT         NOT NULL AUTO_INCREMENT,
+    `id_posto`      INT         NOT NULL,
+    `id_proiezione` INT         NOT NULL,
+    `id_utente`     VARCHAR(45) NOT NULL,
+    `prezzo`        DOUBLE      NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_Biglietto_Utente1_idx` (`id_utente` ASC) VISIBLE,
+    INDEX `fk_Biglietto_Posto1_idx` (`id_posto` ASC) VISIBLE,
+    INDEX `fk_Biglietto_Proiezione1_idx` (`id_proiezione` ASC) VISIBLE,
+    CONSTRAINT `fk_Biglietto_Posto1`
+        FOREIGN KEY (`id_posto`)
+            REFERENCES `cinemadb`.`posto` (`id`),
+    CONSTRAINT `fk_Biglietto_Proiezione1`
+        FOREIGN KEY (`id_proiezione`)
+            REFERENCES `cinemadb`.`proiezione` (`id`),
+    CONSTRAINT `fk_Biglietto_Utente1`
+        FOREIGN KEY (`id_utente`)
+            REFERENCES `cinemadb`.`utente` (`id`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `cinemadb`.`dipendente`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cinemadb`.`dipendente` (
-  `id_utente` VARCHAR(45) NOT NULL,
-  `id_cinema` INT NOT NULL,
-  `ruolo` ENUM('proiezionista', 'maschera') NOT NULL,
-  INDEX `fk_Dipendente_Utente1_idx` (`id_utente` ASC) VISIBLE,
-  INDEX `fk_Dipendente_Cinema1_idx` (`id_cinema` ASC) VISIBLE,
-  CONSTRAINT `fk_Dipendente_Cinema1`
-    FOREIGN KEY (`id_cinema`)
-    REFERENCES `cinemadb`.`cinema` (`id`),
-  CONSTRAINT `fk_Dipendente_Utente1`
-    FOREIGN KEY (`id_utente`)
-    REFERENCES `cinemadb`.`utente` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `cinemadb`.`dipendente`
+(
+    `id_utente` VARCHAR(45)                        NOT NULL,
+    `id_cinema` INT                                NOT NULL,
+    `ruolo`     ENUM ('proiezionista', 'maschera') NOT NULL,
+    INDEX `fk_Dipendente_Utente1_idx` (`id_utente` ASC) VISIBLE,
+    INDEX `fk_Dipendente_Cinema1_idx` (`id_cinema` ASC) VISIBLE,
+    CONSTRAINT `fk_Dipendente_Cinema1`
+        FOREIGN KEY (`id_cinema`)
+            REFERENCES `cinemadb`.`cinema` (`id`),
+    CONSTRAINT `fk_Dipendente_Utente1`
+        FOREIGN KEY (`id_utente`)
+            REFERENCES `cinemadb`.`utente` (`id`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `cinemadb`.`turno`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cinemadb`.`turno` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `inizio` TIME NOT NULL,
-  `fine` TIME NOT NULL,
-  `id_dipendente` VARCHAR(45) NOT NULL,
-  `data` DATE NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Turno_Dipendente1_idx` (`id_dipendente` ASC) VISIBLE,
-  CONSTRAINT `fk_Turno_Dipendente1`
-    FOREIGN KEY (`id_dipendente`)
-    REFERENCES `cinemadb`.`dipendente` (`id_utente`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `cinemadb`.`turno`
+(
+    `id`            INT         NOT NULL AUTO_INCREMENT,
+    `inizio`        TIME        NOT NULL,
+    `fine`          TIME        NOT NULL,
+    `id_dipendente` VARCHAR(45) NOT NULL,
+    `data`          DATE        NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_Turno_Dipendente1_idx` (`id_dipendente` ASC) VISIBLE,
+    CONSTRAINT `fk_Turno_Dipendente1`
+        FOREIGN KEY (`id_dipendente`)
+            REFERENCES `cinemadb`.`dipendente` (`id_utente`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `cinemadb`.`turno_proiezionista`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cinemadb`.`turno_proiezionista` (
-  `turno_id` INT NOT NULL,
-  `sala_id` INT NOT NULL,
-  PRIMARY KEY (`turno_id`),
-  INDEX `fk_turno_proiezionista_turno1_idx` (`turno_id` ASC) VISIBLE,
-  INDEX `fk_turno_proiezionista_sala1_idx` (`sala_id` ASC) VISIBLE,
-  CONSTRAINT `fk_turno_proiezionista_sala1`
-    FOREIGN KEY (`sala_id`)
-    REFERENCES `cinemadb`.`sala` (`id`),
-  CONSTRAINT `fk_turno_proiezionista_turno1`
-    FOREIGN KEY (`turno_id`)
-    REFERENCES `cinemadb`.`turno` (`id`)
-    ON DELETE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `cinemadb`.`turno_proiezionista`
+(
+    `turno_id` INT NOT NULL,
+    `sala_id`  INT NOT NULL,
+    PRIMARY KEY (`turno_id`),
+    INDEX `fk_turno_proiezionista_turno1_idx` (`turno_id` ASC) VISIBLE,
+    INDEX `fk_turno_proiezionista_sala1_idx` (`sala_id` ASC) VISIBLE,
+    CONSTRAINT `fk_turno_proiezionista_sala1`
+        FOREIGN KEY (`sala_id`)
+            REFERENCES `cinemadb`.`sala` (`id`),
+    CONSTRAINT `fk_turno_proiezionista_turno1`
+        FOREIGN KEY (`turno_id`)
+            REFERENCES `cinemadb`.`turno` (`id`)
+            ON DELETE CASCADE
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
-USE `cinemadb` ;
+USE `cinemadb`;
 
 -- -----------------------------------------------------
 -- procedure login
@@ -208,7 +226,7 @@ USE `cinemadb` ;
 
 DELIMITER $$
 USE `cinemadb`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `login`(IN emailParam VARCHAR(255), IN passwordParam VARCHAR(255))
+CREATE PROCEDURE `login`(IN emailParam VARCHAR(255), IN passwordParam VARCHAR(255))
 BEGIN
     SELECT *
     FROM cinemadb.utente
@@ -224,7 +242,7 @@ DELIMITER ;
 
 DELIMITER $$
 USE `cinemadb`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `popola`()
+CREATE PROCEDURE `popola`()
 BEGIN
     CALL cinemadb.popola_utente();
     CALL cinemadb.popola_cinema();
@@ -245,7 +263,7 @@ DELIMITER ;
 
 DELIMITER $$
 USE `cinemadb`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `popola_cinema`()
+CREATE PROCEDURE `popola_cinema`()
 BEGIN
     INSERT INTO `cinemadb`.`cinema` (`nome`, `indirizzo`, `citta`)
     VALUES ('Comunale', 'via recanati 3', 'Recanati');
@@ -261,7 +279,7 @@ DELIMITER ;
 
 DELIMITER $$
 USE `cinemadb`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `popola_dipendente`()
+CREATE PROCEDURE `popola_dipendente`()
 BEGIN
     INSERT INTO `cinemadb`.`dipendente` (`id_utente`, `id_cinema`, `ruolo`)
     VALUES ('0', '1', 'maschera');
@@ -301,7 +319,7 @@ DELIMITER ;
 
 DELIMITER $$
 USE `cinemadb`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `popola_film`()
+CREATE PROCEDURE `popola_film`()
 BEGIN
     declare film int;
     declare ultimo int;
@@ -322,7 +340,7 @@ DELIMITER ;
 
 DELIMITER $$
 USE `cinemadb`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `popola_posti`()
+CREATE PROCEDURE `popola_posti`()
 BEGIN
     DECLARE nsala INT;
     DECLARE counter INT;
@@ -354,42 +372,53 @@ DELIMITER ;
 
 DELIMITER $$
 USE `cinemadb`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `popola_proiezioni`()
+CREATE PROCEDURE `popola_proiezioni`()
 BEGIN
     DECLARE anno VARCHAR(5);
     DECLARE mese VARCHAR(5);
     DECLARE giorno INT;
     DECLARE ora INT;
-    declare film int;
-    declare nfilm int;
-    declare sala int;
-    declare nsale int;
-    set nfilm = (select count(*) from film);
-    set nsale = (select count(*) from sala);
+    DECLARE film INT;
+    DECLARE nfilm INT;
+    DECLARE first_film INT;
+    DECLARE last_film INT;
+    DECLARE sala INT;
+    DECLARE nsale INT;
 
+    SET nsale = (SELECT count(*) FROM sala);
+    SET nfilm = (SELECT count(*) FROM film);
+    SET first_film = (SELECT id FROM film ORDER BY id ASC LIMIT 1);
+    SET last_film = (SELECT id FROM film ORDER BY id DESC LIMIT 1);
     SET anno = '2021';
     SET mese = '05';
     SET giorno = 10;
-    SET ora = 12;
+    SET ora = 15;
 
+    SET film = first_film;
     WHILE giorno < 29
         DO
-            set sala = 1;
-            set film = 15;
-            while sala < (nsale + 1)
-                do
-                    INSERT INTO `cinemadb`.`proiezione` (`id_sala`, `id_film`, `data`, `inizio`, `prezzo_base`)
-                    VALUES (sala, film, concat(anno, '-', mese, '-', giorno), concat(ora, ':00'), 5);
-                    set sala = sala + 1;
-                    set film = film + 1;
-                    if (film < 15 or film > 22) then
-                        set film = 15;
-                    end if;
-                end while;
-            IF ora > 22 THEN
-                SET ora = 12;
-            END IF;
-            SET ora = ora + 1;
+            SET ora = 15;
+            WHILE ora < 22
+                DO
+                    SET sala = 1;
+                    WHILE sala < (nsale + 1)
+                        DO
+                            IF (giorno < 20) THEN
+                                IF (film > (last_film - (nfilm / 2))) THEN
+                                    SET film = first_film;
+                                END IF;
+                            ELSE
+                                IF ((film < (last_film - (nfilm / 2)) + 1) OR film > last_film) THEN
+                                    SET film = first_film + (nfilm / 2);
+                                END IF;
+                            END IF;
+                            INSERT INTO `cinemadb`.`proiezione` (`id_sala`, `id_film`, `data`, `inizio`, `prezzo_base`)
+                            VALUES (sala, film, concat(anno, '-', mese, '-', giorno), concat(ora, ':00'), 5);
+                            SET sala = sala + 1;
+                            SET film = film + 1;
+                        END WHILE;
+                    SET ora = ora + 2;
+                END WHILE;
             SET giorno = giorno + 1;
         END WHILE;
 END$$
@@ -402,7 +431,7 @@ DELIMITER ;
 
 DELIMITER $$
 USE `cinemadb`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `popola_sala`()
+CREATE PROCEDURE `popola_sala`()
 BEGIN
     DECLARE ncinema INT;
     DECLARE cinema INT;
@@ -434,7 +463,7 @@ DELIMITER ;
 
 DELIMITER $$
 USE `cinemadb`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `popola_turni`()
+CREATE PROCEDURE `popola_turni`()
 BEGIN
     DECLARE anno VARCHAR(7);
     DECLARE mese VARCHAR(7);
@@ -448,8 +477,8 @@ BEGIN
     SET nmaschera = (select count(*) from dipendente where ruolo = 'maschera');
     SET anno = '2021';
     SET mese = '05';
-    SET fine = '20:00';
-    SET inizio = '12:00';
+    SET fine = '23:30';
+    SET inizio = '14:30';
     SET giorno = 10;
     WHILE giorno < 28
         DO
@@ -476,7 +505,7 @@ DELIMITER ;
 
 DELIMITER $$
 USE `cinemadb`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `popola_turni_proiezionista`()
+CREATE PROCEDURE `popola_turni_proiezionista`()
 BEGIN
     DECLARE ndipendenti int;
     DECLARE nmaschere int;
@@ -511,7 +540,7 @@ DELIMITER ;
 
 DELIMITER $$
 USE `cinemadb`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `popola_utente`()
+CREATE PROCEDURE `popola_utente`()
 BEGIN
     INSERT INTO `cinemadb`.`utente` (`id`, `nome`, `cognome`, `email`)
     VALUES ('0', 'Fabio', 'Buracchi', 'fb@cinehub.com');
@@ -545,6 +574,6 @@ END$$
 
 DELIMITER ;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+SET SQL_MODE = @OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
