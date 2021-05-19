1630ecfa7e680bdf7583327cd287639bfbd916d9
Ivan <55716224+Ivan-Palm@users.noreply.github.com>
2021-05-19 15:05:30 +0200

    Fix #42
    Fix test

--- app/src/main/java/com/ttbmp/cinehub/app/usecase/buyticket/BuyTicketController.java
+++ app/src/main/java/com/ttbmp/cinehub/app/usecase/buyticket/BuyTicketController.java
@@ -22,6 +22,9 @@ import com.ttbmp.cinehub.app.utilities.request.AuthenticatedRequest;
 import com.ttbmp.cinehub.app.utilities.request.Request;
 import com.ttbmp.cinehub.domain.security.Permission;
 import com.ttbmp.cinehub.domain.ticket.component.Ticket;
+import com.ttbmp.cinehub.domain.ticket.decorator.TicketMagicBox;
+import com.ttbmp.cinehub.domain.ticket.decorator.TicketOpenBar;
+import com.ttbmp.cinehub.domain.ticket.decorator.TicketSkipLine;
 
 /**
  * @author Ivan Palmieri
@@ -151,13 +154,21 @@ public class BuyTicketController implements BuyTicketUseCase {
             var customer = customerRepository.getCustomer(securityService.authenticate("PROJECTIONIST").getId());
             var projection = projectionRepository.getProjection(request.getProjectionId());
             var seat = seatRepository.getSeat(request.getSeatId());
+            ticketRepository.checkTicketExistence(seat,projection); //Controllo l'esistenza passata del biglietto
+
+            //-DECORATOR-//
             var ticket = new Ticket(0, projection.getBasePrice(), customer, seat, projection); // Ticket di base
-            ticket = new TicketOperation().calculatePrice(
-                    ticket,
-                    request.getOpenBarOption(),
-                    request.getMagicBoxOption(),
-                    request.getSkipLineOption()
-            );
+
+            if (Boolean.TRUE.equals(request.getOpenBarOption())) {
+                ticket = new TicketOpenBar(ticket);
+            }
+            if (Boolean.TRUE.equals(request.getMagicBoxOption())) {
+                ticket = new TicketMagicBox(ticket);
+            }
+            if (Boolean.TRUE.equals(request.getSkipLineOption())) {
+                ticket = new TicketSkipLine(ticket);
+            }
+            //---------//
             paymentService.pay(new PayServiceRequest(
                     customer.getEmail(),
                     customer.getName(),
@@ -166,7 +177,7 @@ public class BuyTicketController implements BuyTicketUseCase {
                     request.getCreditCardCvv(),
                     request.getCreditCardExpirationDate()
             ));
-            ticketRepository.checkTicketExistence(seat,projection); //Controllo l'esistenza passata del biglietto
+
             ticketRepository.saveTicket(ticket);
             emailService.sendMail(new EmailServiceRequest(request.getEmail(), "Payment receipt"));
             presenter.presentTicket(TicketDataMapper.mapToDto(ticket));
