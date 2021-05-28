2fcafb34034667da4f491aebdc1524b4dfa821bc
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2021-05-28 17:25:57 +0200

    Revert changes #88

--- ui/desktop/build.gradle.kts
+++ ui/desktop/build.gradle.kts
@@ -9,22 +9,16 @@ dependencies {
     testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.7.0")
     testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine")
 
-    testImplementation("org.testfx", "testfx-core", "4.0.16-alpha")
-    testImplementation("org.testfx", "testfx-junit5", "4.0.16-alpha")
-    testImplementation("org.testfx", "openjfx-monocle", "jdk-11+26")
+    testImplementation("org.testfx:testfx-core:4.0.16-alpha")
+    testImplementation("org.testfx:testfx-junit5:4.0.16-alpha")
+
 }
 
 javafx {
     version = "16"
-    modules("javafx.base", "javafx.controls", "javafx.fxml")
+    modules("javafx.controls", "javafx.fxml")
 }
 
 application {
     mainClass.set("com.ttbmp.cinehub.ui.desktop.CinehubApplication")
-}
-
-tasks.withType<Test>().all {
-    if (project.hasProperty("headless")) {
-        jvmArgs("-Djava.awt.headless=true -Dtestfx.robot=glass -Dtestfx.headless=true -Dprism.order=sw")
-    }
 }
