2beb30331af5ddb4feed1fc4c0735257f7162029
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2021-05-29 11:58:03 +0200

    Refactory #90

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.sonarqube") version "3.1.1"
+    id("org.sonarqube") version "3.2.0"
 }
 
 buildscript {
@@ -7,7 +7,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.sonarsource.scanner.gradle", "sonarqube-gradle-plugin", "3.1.1")
+        classpath("org.sonarsource.scanner.gradle", "sonarqube-gradle-plugin", "3.2.0")
     }
 }
 
