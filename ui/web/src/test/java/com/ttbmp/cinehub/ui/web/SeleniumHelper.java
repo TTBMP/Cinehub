271dd9021e7c0c90466f6a353075b85314181dfb
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2021-06-07 20:12:53 +0200

    Fix #93

---
+++ ui/web/src/test/java/com/ttbmp/cinehub/ui/web/SeleniumHelper.java
@@ -0,0 +1,23 @@
+package com.ttbmp.cinehub.ui.web;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SeleniumHelper {
+
+    private SeleniumHelper() {}
+
+    public static void waitPageToLoad(WebDriver driver) {
+        new WebDriverWait(driver, 10).until(
+                webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+    }
+
+    public static void clickAndWaitPageToLoad(WebDriver driver, WebElement element) {
+        element.click();
+        new WebDriverWait(driver, 10).until(
+                webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+    }
+
+}
