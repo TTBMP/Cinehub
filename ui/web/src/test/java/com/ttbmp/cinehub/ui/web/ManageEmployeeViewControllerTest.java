988fab19956093eebf116547963f51b6bd0e8532
sonMassimo1 <56676941+sonMassimo1@users.noreply.github.com>
2021-06-08 17:54:57 +0200

    fix #93

--- ui/web/src/test/java/com/ttbmp/cinehub/ui/web/ManageEmployeeViewControllerTest.java
+++ ui/web/src/test/java/com/ttbmp/cinehub/ui/web/ManageEmployeeViewControllerTest.java
@@ -1,19 +1,14 @@
 package com.ttbmp.cinehub.ui.web;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
@@ -23,11 +18,11 @@ import java.util.concurrent.TimeUnit;
 import static com.ttbmp.cinehub.ui.web.SeleniumHelper.clickAndWaitPageToLoad;
 import static com.ttbmp.cinehub.ui.web.SeleniumHelper.waitPageToLoad;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
 class ManageEmployeeViewControllerTest {
 
-    public WebElement employee;
     private WebDriver driver;
 
     @BeforeAll
@@ -51,33 +46,20 @@ class ManageEmployeeViewControllerTest {
     }
 
     @Test
-    void foo(){
+    void tableLoaded() {
         loginAsManager();
         var manageEmployeeTab = driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[2]/a"));
         clickAndWaitPageToLoad(driver, manageEmployeeTab);
-        var searchShiftButton = driver.findElement(By.xpath("//*[@id=\"search_shift_cinema\"]"));
-        clickAndWaitPageToLoad(driver, searchShiftButton);
-
-        var assignUsherButton = driver.findElement(By.xpath("//*[@id=\"assignUshShift\"]"));
-       // ((JavascriptExecutor) driver).executeScript("return window.scrollTo(0, " + assignUsherButton.getLocation().y + ");");
-       // ((JavascriptExecutor) driver).executeScript("console.log(\"pippo\");");
-        Actions actions = new Actions(driver);
-        actions.moveToElement(assignUsherButton).build().perform();
-        assignUsherButton.click();
-       // clickAndWaitPageToLoad(driver,assignUsherButton);
-        driver.findElement(By.xpath("//*[@id=\"date\"]")).sendKeys(LocalDate.now().plusDays(1).format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-        driver.findElement(By.xpath("//*[@id=\"start_time\"]")).sendKeys("12:00");
-        driver.findElement(By.xpath("//*[@id=\"end_time\"]")).sendKeys("10:00");
-        driver.findElement(By.xpath("/html/body/main/div/div[2]/form/div/div[7]/a/button")).click();
-        employee = driver.findElement(By.xpath("/html/body/main/div/div/p"));
+        var isPresent = driver.findElements(By.xpath("/html/body/main/div/div[2]/div[1]/table")).size() > 0;
+        assertFalse(isPresent);
     }
 
     private void loginAsManager() {
         var loginButton = driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/form/input"));
-        clickAndWaitPageToLoad(driver,loginButton);
+        clickAndWaitPageToLoad(driver, loginButton);
         driver.findElement(By.xpath("//*[@id=\"floatingInput\"]")).sendKeys("mz@cinehub.com");
         driver.findElement(By.xpath("//*[@id=\"floatingPassword\"]")).sendKeys("123");
         var signInButton = driver.findElement(By.xpath("/html/body/main/form/button"));
-        clickAndWaitPageToLoad(driver,signInButton);
+        clickAndWaitPageToLoad(driver, signInButton);
     }
 }
