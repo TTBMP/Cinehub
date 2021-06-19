31499f9be34f425d7b818920b15108c1876a547c
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2021-06-19 15:20:39 +0200

    Fix #95

--- app/src/main/java/com/ttbmp/cinehub/app/dto/shift/ShiftUsherDto.java
+++ app/src/main/java/com/ttbmp/cinehub/app/dto/shift/ShiftUsherDto.java
@@ -1,12 +1,14 @@
 package com.ttbmp.cinehub.app.dto.shift;
 
 import com.ttbmp.cinehub.domain.shift.UsherShift;
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 /**
  * @author Fabio Buracchi
  */
 @Value
+@EqualsAndHashCode(callSuper = true)
 public class ShiftUsherDto extends ShiftDto {
 
     public ShiftUsherDto(UsherShift shift) {
